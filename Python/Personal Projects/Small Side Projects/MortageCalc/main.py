cfa3a483b8d59542d3f4da8133601e76a46faa72
Igor Romantsov <igorromantsov04@gmail.com>
2025-01-04 22:01:39 -0800

    Update subproject commit hash

--- Python/Personal Projects/Small Side Projects/MortageCalc/main.py
+++ Python/Personal Projects/Small Side Projects/MortageCalc/main.py
@@ -1,3 +1,5 @@
+import math
+
 class MortageInfo:
     def __init__(self):
         self.principal = 0
@@ -17,16 +19,38 @@ class MortageInfo:
         self.term = float(input())
 
     def get_info(self):
-        print("\n")
-        print(f"Principal: ${self.principal}")
+        print(f"\nPrincipal: ${self.principal}")
         print(f"Interest: %{self.interest}")
-        print(f"Term: {self.term} years")
-        print("\n")
+        print(f"Term: {self.term} years\n")
 
 mortage = MortageInfo()
-
+""" workin on this currently
+def compound_calc():
+    user_input = (input("\n\nSelect on of the following:\n 1. Monthly\n2.Weekly\n3.Daily\n4.Continually\n"))
+    inter = mortage.interest / 100 
+    if user_input == '1':
+        monthly = mortage.principal * (1 + (inter / 12)) ** (12 * mortage.term)
+        print(f"\n{monthly:.2f}")
+    elif user_input == '2':
+        weekly = mortage.principal * (1 + (inter / 52)) ** (52 * mortage.term)
+        print(f"{weekly:.2f}")
+    elif user_input == '3':
+        daily = mortage.principal * (1 + (inter / 356)) ** (356 * mortage.term)
+        print(f"{daily:.2f}")
+    elif user_input == '4':
+        continuously = mortage.principal * math.exp(mortage.interest * mortage.term)
+        print(f"{continuously:.2f}")
+"""
 mortage.get_principal()
 mortage.get_interest()
 mortage.get_term()
-mortage.get_info()
 
+while True:
+    user_input = (input("\nSelect on of the following choices: \n1. Display Mortage Info\n2. Compound Calculator\n3. Type 'exit' if done\n"))
+    if user_input == '1':
+        mortage.get_info()
+    elif user_input == '2':
+        compound_calc()
+    elif user_input == '3' or 'exit':
+        print("Cya")
+        break
